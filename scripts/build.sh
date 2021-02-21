206dfc52628d209e380bc95cbbaed7eda9861b07
arnog <arno@arno.org>
2021-02-21 22:09:24 +0100

    chore: prettier build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,6 +5,13 @@ set -o nounset   # abort on unbound variable
 set -o pipefail  # don't hide errors within pipes
 # set -x    # for debuging, trace what is being executed.
 
+export BASENAME="\033[40m"MathJSON"\033[0m" # `basename "$0"`
+export DOT="\033[32m ● \033[0m"
+export CHECK="\033[32m ✔ \033[0m"
+export LINECLEAR="\033[1G\033[2K" # position to column 1; erase whole line
+export ERROR="\033[31m ERROR \033[0m"
+
+
 # Note on the `sed` command:
 # On Linux, the -i switch can be used without an extension argument
 # On macOS, the -i switch must be followed by an extension argument (which can be empty)
@@ -16,7 +23,7 @@ export -f sedi
 
 cd "$(dirname "$0")/.."
 if [ "$#" -gt 1 ]; then
-    echo -e "\033[40m`basename "$0"`\033[0m\033[31m ERROR \033[0m Expected at most one argument: 'development' (default), 'watch' or 'production'"
+    echo -e "$BASENAME$ERROR Expected at most one argument: 'development' (default), 'watch' or 'production'"
     exit 1
 fi
 
@@ -25,9 +32,9 @@ npx check-node-version --package
 
 # If no "node_modules" directory, do an install first
 if [ ! -d "./node_modules" ]; then
-    printf "\033[32m\033[1K ● \033[0Installing dependencies"
+    printf "$BASENAME$DOT Installing dependencies"
     npm install
-    echo -e "\033[32m\033[1K ✔ \033[0Dependencies installed"
+    echo -e "$LINECLEAR$BASENAME$CHECK Dependencies installed"
 fi
 
 # Read the first argument, set it to "development" if not set
@@ -44,14 +51,14 @@ export SDK_VERSION=$(cat package.json \
 | tr -d '[[:space:]]')
 
 # Clean output directories
-printf "\033[32m ● \033[0m Cleaning output directories"
+printf "$BASENAME$DOT Cleaning output directories"
 rm -rf ./dist
 rm -rf ./declarations
 rm -rf ./build
 rm -rf ./coverage
 
 mkdir -p dist
-echo -e "\033[2K\033[80D\033[32m ✔ \033[0m Output directories cleaned out"
+echo -e "$LINECLEAR$BASENAME$CHECK Output directories cleaned out"
 
 if [ "$BUILD" != "production" ]; then
     # Write sentinel file. It will be checked in the pre-push.sh script
@@ -62,37 +69,37 @@ fi
 # Bundle declaration files (.d.ts)
 # Even though we only generate declaration file, the target must be set high-enough
 # to prevent tsc from complaining (!)
-printf "\033[32m ● \033[0m Building declaration files (.d.ts)"
+printf "$BASENAME$DOT Building declaration files (.d.ts)"
 npx tsc --target "es2020" -d --moduleResolution "node" --emitDeclarationOnly --outDir ./dist/types ./src/public.ts 
-echo -e "\033[2K\033[80D\033[32m ✔ \033[0m Declaration files built"
+echo -e "$LINECLEAR$BASENAME$CHECK Declaration files built"
 
 if [ "$BUILD" = "watch" ]; then
     # Do dev build and watch
     printf "\033[32m ● \033[0m Making a \033[33mdevelopment\033[0m build"
     npx rollup --silent --config config/rollup.config.js --watch
-    echo -e "\033[2K\033[80D\033[32m ✔ \033[33mdevelopment\033[0m build done"
+    echo -e "$LINECLEAR$BASENAME$CHECK \033[33mdevelopment\033[0m build done"
 else
     # Do build (development or production)
     printf "\033[32m ● \033[0m Making a \033[33m%s\033[0m build" "$BUILD"
     npx rollup --silent --config config/rollup.config.js
-    echo -e "\033[2K\033[80D\033[32m ✔ \033[33m" $BUILD "\033[0m build done"
+    echo -e "$LINECLEAR$BASENAME$CHECK \033[33m" $BUILD "\033[0m build done"
 
     if [ "$BUILD" = "production" ]; then    
         # Stamp the SDK version number
-        printf "\033[32m ● \033[0m Stamping output files"
+        printf "$BASENAME$DOT Stamping output files"
         find ./dist -type f \( -name '*.js' -o -name '*.js' \) -exec bash -c 'sedi s/{{SDK_VERSION}}/$SDK_VERSION/g {}' \;
         find ./dist -type f -name '*.d.ts' -exec bash -c 'sedi "1s/^/\/\* $SDK_VERSION \*\/$(printf '"'"'\r'"'"')/" {}' \;
         find ./dist -type f -name '*.d.ts' -exec bash -c 'sedi "s/{{SDK_VERSION}}/$SDK_VERSION/" {}' \;
-        echo -e "\033[2K\033[80D\033[32m ✔ \033[0m Output files stamped"
+        echo -e "$LINECLEAR$BASENAME$CHECK Output files stamped"
 
         # Linting
         # echo -e "\033[40m`basename "$0"`\033[0m 🚀 Linting"
         # npm run lint
 
         # Run test suite
-        printf "\033[32m ● \033[0m Running test suite"
+        printf "$BASENAME$DOT Running test suite"
         npx jest --config ./config/jest.config.js ./test --silent --reporters jest-silent-reporter
-        echo -e "\033[2K\033[80D\033[32m ✔ \033[0m Test suite complete"
+        echo -e "$LINECLEAR$BASENAME$CHECK Test suite complete"
     fi
 fi
 
