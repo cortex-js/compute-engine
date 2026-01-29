6332482b4dd21744ced9bf31e229c5a4c52d89e9
Arno Gourdol <arno@arno.org>
2026-01-29 15:12:48 -0800

    chore: speed up targeted test suite running

--- scripts/test.sh
+++ scripts/test.sh
@@ -12,18 +12,18 @@ VARIANT="${1-coverage}"
 
 export TEST="true"
 
-echo -e "\n🧐 Running Declaration Type test suite..."
-
-npx tsc --noEmit --baseUrl ./dist/types ./test/public-ts-declarations/main.ts || exit_code=$?
-
-echo -e "\033[2K\033[80D\033[32m✔ \033[0m Declaration Type test suite complete"
-
-
 if [ "$VARIANT" = "coverage" ] || [ "$VARIANT" = "-coverage" ]; then
+    echo -e "\n🧐 Running Declaration Type test suite..."
+    npx tsc --noEmit --baseUrl ./dist/types ./test/public-ts-declarations/main.ts || exit_code=$?
+    echo -e "\033[2K\033[80D\033[32m✔ \033[0m Declaration Type test suite complete"
     npx jest --config ./config/jest.config.cjs --coverage --no-cache
 elif [ "$VARIANT" = "snapshot" ]  || [ "$VARIANT" = "-snapshot" ]; then
+    echo -e "\n🧐 Running Declaration Type test suite..."
+    npx tsc --noEmit --baseUrl ./dist/types ./test/public-ts-declarations/main.ts || exit_code=$?
+    echo -e "\033[2K\033[80D\033[32m✔ \033[0m Declaration Type test suite complete"
     npx jest --config ./config/jest.config.cjs  --updateSnapshot
 else
+    # Skip Declaration Type test for specific test files - run it directly
     echo "${1}".test.ts
     npx jest --config ./config/jest.config.cjs ./test/"${1}" --verbose false
 fi
